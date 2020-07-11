611a557c83b4a382fd1388887fbd3155ba4e208f
Coop <61427314+codelucid@users.noreply.github.com>
2020-07-11 09:44:36 -0400

    removed the models and connection syntax from server.js file

--- Develop/models/Index.js
+++
@@ -1,6 +0,0 @@
-// Exporting an object containing all of our models
-
-module.exports = {
-    Resistance: require("./Resistance"),
-    Cardio: require("./Cardio")
-};
--- Develop/server.js
+++ Develop/server.js
@@ -13,14 +13,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+// This will also connect to Heroku
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
-
-// Heroku Connection
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect(MONGODB_URI);
 
 // routes
 app.use(require("./routes/api.js"));
